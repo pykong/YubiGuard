e5b8b4a2bdfb73f10968509e6ccfad5a0b0a572a
bfelder <ben.felder@gmx.de>
2016-07-24 23:42:29 +0200

    Update YubiLock_installer.sh

--- YubiLock_installer.sh
+++ YubiLock_installer.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 # does not yet perform key binding, which hence has to performed manually
 
-DIR = ~/.YubiGuard
-mkidr $DIR && cd $DIR
-wget --inet4-only github.com/bfelder/YubiGuard/zipball/master
-tar -xzvf master.zip
-rm -f master.zip
-sudo chmod +x YubiGuard.py
-sudo chmod +x YubiGuard_trigger.sh
+# installer script, not yet read to use, please use manual installation !!!
+# work in progress ...
 
-cat > /etc/init/myjob.conf << EOF
-description     "YubiGuard"
-start on startup
-task
-exec /path/to/my/script.sh
-EOF
+#DIR = ~/.YubiGuard
+#mkidr $DIR && cd $DIR
+#wget --inet4-only github.com/bfelder/YubiGuard/zipball/master
+#tar -xzvf master.zip
+#rm -f master.zip
+#sudo chmod +x YubiGuard.py
+#sudo chmod +x YubiGuard_trigger.sh
 
-./YubiGuard.py
+#cat > /etc/init/myjob.conf << EOF
+#description     "YubiGuard"
+#start on startup
+#task
+#exec /path/to/my/script.sh
+#EOF
+
+#./YubiGuard.py
