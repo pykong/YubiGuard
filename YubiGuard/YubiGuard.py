780d246a2b0102853eb0a829bf321dfdf86693ad
bfelder <ben.felder@gmx.de>
2016-09-01 20:51:10 +0200

    Add files via upload

--- YubiGuard/YubiGuard.py
+++ YubiGuard/YubiGuard.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# YubiGuard VERSION 0.9
+# YubiGuard VERSION 0.9.1
 # LICENSE: GNU General Public License v3.0
 # https://stackoverflow.com/questions/285716/trapping-second-keyboard-input-in-ubuntu-linux
 # shell command for pushing ZMQ: echo -e $(printf '\\x01\\x00\\x%02x\\x00%s' $((1 + ${#MSG})) "$MSG") | nc -q1 $IP $PORT
@@ -11,6 +11,7 @@ import re
 import time
 import zmq
 import subprocess
+import argparse
 
 from multiprocessing import Process
 from multiprocessing.queues import Queue
@@ -54,9 +55,36 @@ URL = "tcp://{IP}:{PORT}".format(
 
 
 # static methods:
-def shell(cmd):
-    stdout = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    return stdout
+def shell_this(cmd):
+    """from http://blog.kagesenshi.org/2008/02/teeing-python-subprocesspopen-output.html
+    """
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout = []
+    while True:
+        line = p.stdout.readline()
+        stdout.append(line)
+        # print line,
+        if line == '' and p.poll() is not None:
+            break
+    return ''.join(stdout)
+
+
+def get_scrlck_cmd():
+    """
+    needs screensaver to run for the session:
+    https://askubuntu.com/questions/7776/how-do-i-lock-the-desktop-screen-via-command-line
+    """
+    cmd_d = dict(
+        cinammon='cinammon-screensaver-command -l',
+        gnome='gnome-screensaver-command -l',
+        mate='mate-screensaver-command -l',
+        xfce='xflock4'
+    )
+    sh_out = shell_this("ls /usr/bin/*session")
+
+    for k, v in cmd_d.iteritems():
+        if k in sh_out:
+            return v
 
 
 class PanelIndicator(object):
@@ -70,7 +98,7 @@ class PanelIndicator(object):
 
     def run_pi(self):
         # suppresses error: Couldn't connect to accessibility bus: Failed to connect to socket:
-        subprocess.Popen(["export", "NO_AT_BRIDGE=1"], shell=True)
+        shell_this("export NO_AT_BRIDGE=1")
 
         # listener loop for icon switch signals
         ui_thread = Thread(target=self.update_icon)
@@ -105,7 +133,7 @@ class PanelIndicator(object):
                    "kde-open {HELP_URL} || " \
                    "notify-send --expire-time=7000" \
                    " \"Could not open YubiGuard web help:\" \"{HELP_URL}\"".format(HELP_URL=HELP_URL)
-        subprocess.Popen(help_cmd, shell=True)
+        shell_this(help_cmd)
 
     def quit(self, *arg):
         print('Quitting Gtk.')
@@ -173,7 +201,8 @@ class AsynchronousFileReader(Thread):
 
 
 class YubiGuard:
-    def __init__(self):
+    def __init__(self, scrlck_mode=False):
+        self.scrlck_mode = scrlck_mode
 
         self.id_q = Queue()
         self.on_q = Queue()
@@ -212,17 +241,15 @@ class YubiGuard:
     def get_ids(self):
         old_id_l = []
         no_key = True
+        pat = re.compile(r"(?:Yubikey.*?id=)(\d+)", re.IGNORECASE)
         while True:
             new_id_l = []
             # get list of xinput device ids and extract those of YubiKeys:
-            pat = re.compile(r"(?:Yubikey.*?id=)(\d+)", re.IGNORECASE)
-            list_cmd = 'xinput list'
-            xinput = shell(list_cmd)
-            for line in xinput.stdout:
-                match = re.search(pat, line)
-                if match:
-                    yubi_id = match.groups()
-                    new_id_l.extend(yubi_id)
+
+            xinput = shell_this('xinput list')
+            matches = re.findall(pat, xinput)
+            new_id_l.extend(matches)
+            new_id_l.sort()  # sometimes matches occur in different order to prevent false trigger sorting is required
 
             if not new_id_l and not no_key:
                 self.pi_q.put(NOKEY_SIGNAL)
@@ -234,73 +261,87 @@ class YubiGuard:
                 no_key = False
                 # notify:
                 msg_cmd = "notify-send --expire-time=2000 'YubiKey(s) detected.'"
-                shell(msg_cmd)
+                shell_this(msg_cmd)
 
             if new_id_l != old_id_l:
-                print('Change in YubiKey ids detected.')
+                print('Change in YubiKey ids detected. From {} to {}.'.format(old_id_l, new_id_l))
                 self.id_q.put(new_id_l)
+
+                # lock screen if screenlock mode is enabled and YubiKey is removed:
+                if self.scrlck_mode and len(new_id_l) < len(old_id_l):
+                    print('Locking screen.')
+                    shell_this(get_scrlck_cmd())  # execute screen lock command
 
             old_id_l = new_id_l
 
             time.sleep(.01)
 
-    def unlock_keys(self, id_l):
-        shell('; '.join(["xinput --enable {}".format(cs_id) for cs_id in id_l]))
-        print('Unlocking YubiKey(s).')
-        # switch to ON icon:
-        self.pi_q.put(ON_SIGNAL)
+    def turn_keys(self, id_l, lock=True):  # problem of value loss of cs_id_l found in this function
+        tk_id_l = id_l
+        if lock:
+            print('Locking YubiKey(s).')
+            state_flag = '0'
+            self.pi_q.put(OFF_SIGNAL)
+        else:
+            print('Unlocking YubiKey(s).')
+            state_flag = '1'
+            self.pi_q.put(ON_SIGNAL)
 
-    def lock_keys(self, id_l):
-        shell('; '.join(["xinput --disable {}".format(cs_id) for cs_id in id_l]))
-        print('Locking YubiKey(s).')
-        # switch to OFF icon:
-        self.pi_q.put(OFF_SIGNAL)
+        shell_this(
+            '; '.join(["xinput set-int-prop {} \"Device Enabled\" 8 {}".format(tk_id, state_flag) for tk_id in tk_id_l])
+        )
+
+    def check_state(self, check_id_l):
+        # check if all states have indeed changed:
+        pat = re.compile(r"(?:Device Enabled.+?:).?([01])", re.IGNORECASE)
+        # check if state has indeed changed:
+
+        for tk_id in check_id_l:
+            sh_out = shell_this('xinput list-props {}'.format(tk_id))
+            match = re.search(pat, sh_out)
+            if match:
+                if match.group(1) != '0':
+                    return False
+
 
     def change_state(self):
-        cs_is_l = []
+        cs_id_l = []
         cs_signal = ''
-        lock_done = False
 
         while True:
             # retrieve input from queues
             while self.id_q.qsize() > 0:
-                cs_is_l = self.id_q.get()
+                cs_id_l = self.id_q.get()
                 lock_done = False  # after any change deactivate again
 
             while self.on_q.qsize() > 0:
                 cs_signal = self.on_q.get()
+                if cs_signal == EXIT_SIGNAL:  # not accepting any more more signals after EXIT_SIGNAL is received
+                    self.turn_keys(cs_id_l, lock=False)
+                    sys.exit(0)
 
-            # unlock / lock:
-            if cs_signal == EXIT_SIGNAL:
-                if cs_is_l:
-                    print('Exiting gracefully and cleaning up:'),
-                    self.unlock_keys(cs_is_l)
-                sys.exit(0)
-
-            elif cs_is_l:
+            # lock/unlock
+            if cs_id_l:
                 if cs_signal == ON_SIGNAL:
-                    self.unlock_keys(cs_is_l)
+                    self.turn_keys(cs_id_l, lock=False)
 
-                    mon_thread = Thread(target=self.yk_monitor, args=(cs_is_l,))
+                    mon_thread = Thread(target=self.yk_monitor, args=(cs_id_l,))
                     mon_thread.start()
                     mon_thread.join()
 
                     # putting in separator, nullifying all preceding ON_SIGNALS to prevent possible over-triggering:
                     self.on_q.put('')
 
-                    # setting loop up for deactivation
-                    lock_done = False
-
-                if not lock_done:  # initial disabling
-                    self.lock_keys(cs_is_l)
-                    lock_done = True
+                elif self.check_state(cs_id_l) is False:  # lock keys if they are unlocked
+                    self.turn_keys(cs_id_l, lock=True)
 
             cs_signal = ''  # reset state to prevent continued unlocking/locking
             time.sleep(.001)
 
     def yk_monitor(self, mon_l):
         # forming command to run parallel monitoring processes
-        monitor = shell(' & '.join(["xinput test {}".format(y_id) for y_id in mon_l]))
+        mon_cmd = ' & '.join(["xinput test {}".format(y_id) for y_id in mon_l])
+        monitor = subprocess.Popen(mon_cmd, shell=True, stdout=subprocess.PIPE)
 
         stdout_queue = Queue()
         stdout_reader = AsynchronousFileReader(monitor.stdout, stdout_queue)
@@ -310,9 +351,7 @@ class YubiGuard:
         timestamp = time.time()
         while not stdout_reader.eof and time.time() - timestamp < TIMEOUT:
             while stdout_queue.qsize() > 0:
-                p = stdout_queue.get()  # emptying queue
-                if 'press' in p:
-                    print(p)
+                stdout_queue.get()  # emptying queue
                 triggered = True
                 time.sleep(.01)
             if triggered:
@@ -320,16 +359,27 @@ class YubiGuard:
                 break
 
             time.sleep(.001)
+        if not triggered:
+            print('No YubiKey triggered. Timeout.')
 
 
 # FIRING UP YUBIGUARD --------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    if ('-t' or '--trigger') in sys.argv:
+    parser = argparse.ArgumentParser(description='YubiGuard help')
+    parser.add_argument('-t', nargs='?', default="", help='activates YubiGuard.py as trigger')
+    parser.add_argument('-l', nargs='?', default="", help='lock screen if all YubiKey(s) are removed')
+    args = parser.parse_args()
+
+    if args.t is None:
         print("Sending ON_SIGNAL.")
         context = zmq.Context()
         zmq_socket = context.socket(zmq.PUSH)
         zmq_socket.connect(URL)
         zmq_socket.send(ON_SIGNAL)
+
+    elif args.l is None:
+        print("Starting YubiGuard in screen lock mode.")
+        yg = YubiGuard(scrlck_mode=True)
     else:
         print("Starting YubiGuard.")
         yg = YubiGuard()
