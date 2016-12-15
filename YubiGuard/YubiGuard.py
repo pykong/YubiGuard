b7019a9c7c6c86ed3021268f632b30714f48aaa6
Frederik Braun <fbraun+gh@mozilla.com>
2016-12-15 10:05:09 +0100

    Fix PyGIWarning
    Warning is: PyGIWarning: AppIndicator3 was imported without specifying a version
    first. Use gi.require_version('AppIndicator3', '0.1') before import to
    ensure that the right version gets loaded

--- YubiGuard/YubiGuard.py
+++ YubiGuard/YubiGuard.py
@@ -17,12 +17,13 @@ from multiprocessing.queues import Queue
 from threading import Thread
 
 import gi.repository
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+
 import zmq
 from gi.repository import AppIndicator3 as AppIndicator
 from gi.repository import Gtk
 
-gi.require_version('Gtk', '3.0')
-gi.require_version('AppIndicator3', '0.1')
 
 # change working dir to that of script:
 abspath = os.path.abspath(__file__)
